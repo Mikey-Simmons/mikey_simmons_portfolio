4567b5911a3a3e84cb350b44a0076f054f4d1b83
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2021-09-09 15:13:34 -0700

    Update aboutMe.js

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -3,7 +3,7 @@ import mikey1 from '../src/imgs/mikey.jpg'
 const aboutMe =  <div>
     <div  className="about_me">
       
-      <p className="aboutme" >I am a passionate web developer who loves learning new technologies and implementing them into real world solutions.  I am proficient in MERN, Python, and Java.   My curiosity has driven me to become a lifelong learner.    I started and finished an extremely difficult 14 week coding boot camp called Coding Dojo in the middle of the Covid-19 pandemic.  Although it was challenging, I proved to myself that I can strive under extremely difficult circumstances and I gained an insane amount of technical knowledge.  Having previously worked in sales, 
+      <p className="about" >I am a passionate web developer who loves learning new technologies and implementing them into real world solutions.  I am proficient in MERN, Python, and Java.   My curiosity has driven me to become a lifelong learner.    I started and finished an extremely difficult 14 week coding boot camp called Coding Dojo in the middle of the Covid-19 pandemic.  Although it was challenging, I proved to myself that I can strive under extremely difficult circumstances and I gained an insane amount of technical knowledge.  Having previously worked in sales, 
         I love to work with people and prefer working in a team like environment.
       </p>
     </div>
