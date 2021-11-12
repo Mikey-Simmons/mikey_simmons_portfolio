ac70d5668e41c42823fe015231e40c5b3a306380
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2021-11-12 12:25:58 -0800

    Update aboutMe.js

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -4,9 +4,11 @@ const aboutMe =  <div>
     <div  className="about_me">
       
       <p className="about" >I am a passionate web developer who loves learning new technologies and implementing them into real world solutions.  I am proficient in MERN, Python, and Java.   My curiosity has driven me to become a lifelong learner.    I started and finished an extremely difficult 14 week coding boot camp called Coding Dojo in the middle of the Covid-19 pandemic.  Although it was challenging, I proved to myself that I can strive under extremely difficult circumstances and I gained an insane amount of technical knowledge.  Having previously worked in sales, 
-        I love to work with people and prefer working in a team like environment.
+        I love to work with people and prefer working in a team like environment.  Outside of work I am an avid gamer and I have a passion for basketball.  
       </p>
+
     </div>
+
 </div>;
 
 export default aboutMe;
