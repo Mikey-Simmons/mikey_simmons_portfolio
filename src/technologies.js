ce319e9a3f074d68ad24c2ba14337228b14e3407
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-03-17 12:34:31 -0700

    Update technologies.js

--- src/technologies.js
+++ src/technologies.js
@@ -1,23 +1,4 @@
-import react1 from "./imgs/react.png"
-import python from "./imgs/python.gif"
-import java from "./imgs/java.jpg"
-import js from "./imgs/js.png"
-import django from "./imgs/djagno.png"
-import html from "./imgs/html.png"
-import css from "./imgs/css.png"
-import node from "./imgs/node.png"
-import spring from "./imgs/spring.png"
-import express from "./imgs/express.png"
-import mongo from "./imgs/mongo.gif"
-import github from "./imgs/github.png"
-import sqlite from "./imgs/sqlite.gif"
-import selenium from "./imgs/seleneium.gif"
-import mysql from "./imgs/mysql.png"
-import git from "./imgs/git.png"
-import aws from "./imgs/aws.gif"
-import vsc from "./imgs/vsc.png"
-import rest from "./imgs/rest.png"
-import heroku from "./imgs/heroku.png"
+
 
 const techologies =
     <div className="techs">
